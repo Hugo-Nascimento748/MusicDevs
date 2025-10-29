4711f4f5df74658afb875b77c3a9448af2fa5e96
Hugo-Nascimento748 <nascimentohugo748@gmail.com>
2025-10-28 21:00:02 -0300

    Model Musica

---
+++ src/MusicaModel.ts
@@ -0,0 +1,20 @@
+class Musica{
+    private nome: string;
+    private banda: string;
+    private produtora: string;
+    
+    constructor (nome: string, banda: string, produtora: string) {
+        this.nome = nome;
+        this.banda = banda;
+        this.produtora = produtora;
+    }
+
+    public setNome(nome: string) {this.nome = nome;}
+    public getNome(){return this.nome;}
+
+    public setBanda(banda: string) {this.banda = banda;}
+    public getBanda() {return this.banda;}
+
+    public setProdutora(produtora: string) {this.produtora = produtora;}
+    public getProdutora() {return this.produtora;}
+}
